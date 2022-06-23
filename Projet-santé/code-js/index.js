342ae783360ce6e6d79b0c917e01016b2fffe24c
Yobson2 <yobouelouiscarlos@gmail.com>
2022-06-23 08:46:03 -0700

    projet du mois de juin 2022

---
+++ Projet-santé/code-js/index.js
@@ -0,0 +1,13 @@
+let contentTitre=document.querySelector('.titre');
+let titre=document.querySelector("h1").innerText;
+console.log(titre)
+let index=0;
+
+// function afficher(){
+//     contentTitre.textContent=titre.slice(0,index);
+//     index++;
+//     if(index>titre.length){
+//         index=0;
+//     }
+// };
+// setInterval(afficher,100)
